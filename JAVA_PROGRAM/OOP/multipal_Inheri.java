8e50b1c7b05c10c720c68a2d18d148bed4bc09a9
Rohan-Wala <rohanwala529@gmail.com>
2022-01-07 17:58:00 +0530

    multipal inheritance

---
+++ JAVA_PROGRAM/OOP/multipal_Inheri.java
@@ -0,0 +1,29 @@
+class BCCI{
+    String name = "rohan";
+    int age = 21;
+    BCCI(){
+        System.out.println("in BCCI constructor");
+    }
+}
+class IPL extends BCCI{
+    IPL(){
+        System.out.println("in ILP constructor");
+    }
+    IPL(BCCI  obj){
+        System.out.println("in parameterize constructor");
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+
+    }
+}
+class Demo{
+    public static void main(String[] args) {
+        BCCI b1 = new BCCI();
+
+        IPL i1 = new IPL();
+        
+        IPL i2 = new IPL(b1);
+
+        
+    }
+}
